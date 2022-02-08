6bfadadd2c5dafc952e234b2ba6db98bc2d08677
aravi <aravi@DESKTOP-LBACVI0>
2022-02-08 08:07:56 +0530

    Add project files.

---
+++ ProjectA/Controllers/WeatherApiController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectA.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WeatherApiController : ControllerBase
+    {
+        // GET: api/<WeatherApiController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<WeatherApiController>/5
+        [HttpGet]
+        [Route("/GetWeather")]
+        public string GetWeather()
+        {
+
+            var client = new RestClient("http://api.openweathermap.org/data/2.5/weather?lat=9.9312&lon=76.2673&appid=5c846ccb12d264eb0d469cd27ebf6ce9");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            //response.Content.
+            return (response.Content);
+        }
+
+        // POST api/<WeatherApiController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<WeatherApiController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<WeatherApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
